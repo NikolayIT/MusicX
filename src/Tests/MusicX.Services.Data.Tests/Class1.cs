ff9ed36da7523fc621b31eff29fb42ab6e13ddda
Nikolay Kostov <admin@nikolay.it>
2018-08-15 17:03:52 +0300

    Added unit tests library MusicX.Services.Data.Tests

---
+++ src/Tests/MusicX.Services.Data.Tests/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace MusicX.Services.Data.Tests
+{
+    using Xunit;
+
+    public class Class1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}
