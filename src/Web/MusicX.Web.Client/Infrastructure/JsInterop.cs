a4736dfbd5eb587a6377e693a8bc628cfbd31abd
Nikolay Kostov <admin@nikolay.it>
2018-08-30 18:07:00 +0300

    Created token manager JS code in jsInterop.js. Created JsInterop static class to help with JS invocation. Load token information on application startup.

---
+++ src/Web/MusicX.Web.Client/Infrastructure/JsInterop.cs
@@ -0,0 +1,27 @@
+﻿namespace MusicX.Web.Client.Infrastructure
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.JSInterop;
+
+    public static class JsInterop
+    {
+        public static Task<bool> SaveToken(string token)
+        {
+            // Implemented in jsInterop.js
+            return JSRuntime.Current.InvokeAsync<bool>("tokenManager.save", token);
+        }
+
+        public static Task<string> ReadToken()
+        {
+            // Implemented in jsInterop.js
+            return JSRuntime.Current.InvokeAsync<string>("tokenManager.read");
+        }
+
+        public static Task<bool> DeleteToken()
+        {
+            // Implemented in jsInterop.js
+            return JSRuntime.Current.InvokeAsync<bool>("tokenManager.delete");
+        }
+    }
+}
---
+++ src/Web/MusicX.Web.Client/wwwroot/js/jsInterop.js
@@ -0,0 +1,18 @@
+﻿// TODO: use another storage method. Read this: https://dev.to/rdegges/please-stop-using-local-storage-1i04
+window.tokenManager = {
+    save: function (token) {
+        window.localStorage.setItem('jwt', token);
+        console.log("Authentication token has been stored.");
+        return true;
+    },
+    read: function () {
+        var token = window.localStorage.getItem('jwt');
+        console.log(token ? "Authentication token read from storage." : "No authentication token found in storage.");
+        return token;
+    },
+    delete: function () {
+        window.localStorage.removeItem('jwt');
+        console.log("Authentication token has been deleted.");
+        return true;
+    }
+};
