db8bc945060fc6e9add053bc32f9f36f17c9b810
Nikolay Kostov <admin@nikolay.it>
2018-09-06 23:10:08 +0300

    Work in progress: ICustomMapping.cs and CustomMappingPair.cs

---
+++ src/MusicX.Common/Mapping/CustomMappingPair.cs
@@ -0,0 +1,20 @@
+﻿namespace MusicX.Common.Mapping
+{
+    using System;
+    using System.Linq.Expressions;
+
+    public class CustomMappingPair<TSource, TDestination>
+    {
+        public CustomMappingPair(
+            Expression<Func<TSource, object>> sourceMember,
+            Expression<Func<TDestination, object>> destinationMember)
+        {
+            this.SourceMember = sourceMember;
+            this.DestinationMember = destinationMember;
+        }
+
+        public Expression<Func<TSource, object>> SourceMember { get; set; }
+
+        public Expression<Func<TDestination, object>> DestinationMember { get; set; }
+    }
+}
---
+++ src/MusicX.Common/Mapping/ICustomMapping.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicX.Common.Mapping
+{
+    using System.Collections.Generic;
+
+    public interface ICustomMapping<TSource, TDestination>
+    {
+        IEnumerable<CustomMappingPair<TSource, TDestination>> GetCustomMappings();
+    }
+}
