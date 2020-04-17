534fe8e551cba3fdff43b1d2eec24ff7ea74638c
Nikolay Kostov <admin@nikolay.it>
2020-04-17 08:08:57 +0300

    Update to Blazor WebAssembly 3.2.0 Preview 4

--- src/Web/MusicX.Web.Client/Program.cs
+++ src/Web/MusicX.Web.Client/Program.cs
@@ -1,5 +1,7 @@
 ﻿namespace MusicX.Web.Client
 {
+    using System;
+    using System.Net.Http;
     using System.Threading.Tasks;
 
     using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -14,7 +16,8 @@
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddBaseAddressHttpClient();
+            builder.Services.AddSingleton(
+                new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddSingleton<IApplicationState, ApplicationState>();
             builder.Services.AddSingleton<IMediaPlayer, MediaPlayer>();
             builder.Services.AddTransient<IApiClient, ApiClient>();
